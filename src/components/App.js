5b096a9927962a1538ba6808872892787bb5d26c
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-03-29 21:40:12 +0530

    Added Goals, Todos, List and App components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+import React from 'react';
+import { connect } from 'react-redux';
+
+import ConnectedTodos from './Todos';
+import ConnectedGoals from './Goals';
+import handleInitialData from '../actions/shared';
+
+class App extends React.Component {
+  componentDidMount() {
+      const { dispatch } = this.props;
+
+      dispatch(handleInitialData());
+  }
+
+  render() {
+      const { loading } = this.props;
+
+      if(loading === true){
+          return <h3>Loading</h3>
+      }
+
+      return (
+          <div>
+              <ConnectedTodos />
+              <ConnectedGoals />    
+          </div>
+      )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
